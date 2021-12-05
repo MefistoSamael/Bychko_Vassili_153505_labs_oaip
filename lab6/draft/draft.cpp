5fa4dcd47a440d90a270bd83351651690c8126d5
MefistoSamael <vasa060504@mail.ru>
2021-12-05 23:47:33 +0300

    lab6

--- lab6/draft/draft.cpp
+++ lab6/draft/draft.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 double func(double a, double b)
@@ -8,15 +9,8 @@ double func(double a, double b)
 
 int main()
 {
-	int a = 7;
-	int b = 5;
-	double* ptr = &func();
-	double** ptr2 = &ptr;
-	cout << ptr2;
-	cout << ptr;
-	cout << func(a, b);
-
-
+	double a = 5, b = 3;
+	assert(func(a, b) == a+b+1 && "y are gay");
 
 	return 0;
 }
