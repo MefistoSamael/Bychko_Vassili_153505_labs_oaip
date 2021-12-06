9ce671ee0b30044ccfb8f52bff8e7abd9842bbfc
MefistoSamael <vasa060504@mail.ru>
2021-12-06 17:40:13 +0300

    draft

--- lab6/draft/draft.cpp
+++ lab6/draft/draft.cpp
@@ -2,15 +2,53 @@
 #include <cassert>
 using namespace std;
 
-double func(double a, double b)
+void insert(char* str, char* substr, char symb)
 {
-	return (a + b);
+	bool B = 1;
+	int index = 0;
+	char* newstr = (char*)calloc(100, sizeof(char));
+	int k = strlen(substr) - 1;
+	for (int i = 0; i < 100; i++)//заполнение нового массива
+	{
+		for (; str[1] != ' '; i++)
+		{
+			newstr[i] = str[i];
+		}
+		if (newstr[i] = substr[k])
+		{
+			for (; k >= 0; k++)
+			{
+				if (newstr[i] != substr[k])
+				{
+					break;
+					B = 0;
+				}
+
+			}
+		}
+	}
+	free(str);
+	str = newstr;
+
 }
 
 int main()
 {
-	double a = 5, b = 3;
-	assert(func(a, b) == a+b+1 && "y are gay");
+	char* str = (char*)malloc(20* sizeof(char));
+	cin.getline(str, 10);
+	char* substr = (char*)malloc(20* sizeof(char));
+	cin.getline(substr, 10);
+	cout << "\nstrlen = " << strlen(substr);
+	cout <<"str = " << str;
+	cout << endl <<"substr = " <<substr << endl;
+	cout << "Enter symb = ";
+	char symb;
+	cin >> symb;
+	insert(str, substr, symb);
+	cout << "newstr = " << str;
+
+	free(str);
+	free(substr);
 
 	return 0;
 }
