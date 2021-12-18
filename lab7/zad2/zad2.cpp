e0bfbfcb6c91003a7e3d8ce0a79e7d17461f435d
MefistoSamael <92307228+MefistoSamael@users.noreply.github.com>
2021-12-18 14:20:01 +0300

    zad2

--- lab7/zad2/zad2.cpp
+++ lab7/zad2/zad2.cpp
@@ -1,44 +1,4 @@
-<<<<<<< HEAD
-﻿#include <iostream>
-using namespace std;
 
-int main()
-{
-	int numb;
-	cin >> numb;
-	int n;
-	if (numb < 0)
-	{
-		cout << "1";
-		numb *= 1;
-	}
-	for (n = 0; pow(2, n) < numb; n++);
-	int n2 = pow(2, n);
-	bool B = 0;
-	if (n2 == numb)
-	{
-		B = 1;
-	}
-	int j = 0;
-	for (int i = numb; i > 0;)
-	{
-		j = i / n2;
-		i -= j * n2;
-		cout << j;
-		n2 /= 2;
-	}
-	if (B)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			cout << "0";
-		}
-	}
-
-	return 0;
-}
-
-=======
 #include <iostream>
 using namespace std;
 
@@ -79,4 +39,4 @@ int main()
 
 	return 0;
 }
->>>>>>> 0eabba80f1ca109c4c724461a69d2130ee9b67e7
+
