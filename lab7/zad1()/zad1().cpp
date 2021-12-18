13f895f6bf094ea3b522736e1198059ddccfc2c5
MefistoSamael <vasa060504@mail.ru>
2021-12-18 13:53:48 +0300

    zad1

---
+++ lab7/zad1()/zad1().cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int numb(char symb);//замена символа на число
+char numbtochar(int numb)
+{
+	if (numb == 0) return '0';
+	else if (numb == 1) return '1';
+	else if (numb == 2) return '2';
+	else if (numb == 3) return '3';
+	else if (numb == 4) return '4';
+	else if (numb == 5) return '5';
+	else if (numb == 6) return '6';
+	else if(numb == 7) return '7';
+	else if(numb == 8) return '8';
+	else if(numb == 9) return '9';
+}
+
+int main()
+{
+	char a = 0;
+	char b = 0;
+	char c = 0;
+	long long numb10 = 0;
+	char first;
+	int stepen = 0;
+
+	cin >> first;
+	cout << "numb of symb = " << cin.rdbuf()->in_avail() << endl;
+
+	//перевод в десятичную СС
+	numb10 = numb(first) * pow(13, cin.rdbuf()->in_avail() - 1);//посокльку число было введено заранее проделаем один шаг вне цикла
+	stepen = (cin.rdbuf()->in_avail()) - 2;
+	while (a != '\n' && stepen >=0)
+	{
+		cin >> a;
+		numb10 += numb(a) * pow(13, stepen);
+		stepen--;
+	}
+	cout << "numb10 = " << numb10 << endl;
+
+	
+	//перевод в троичную СС
+	//норм реализация этой части ниже)))))))
+	cout << endl;
+	for (long long i = numb10; i > 0;)
+	{
+		int temp = i % 3;
+		i /= 3;
+		cout << temp;
+
+	}
+	cout << " = 3bmun";
+
+	/*long long n = 0;
+	cout << "numb3 = ";
+
+
+	int j = 0;
+	long long j3 = 0;
+	for (j = 0; pow(3, j) < numb10; j++) j3 = pow(3, j);
+	for (long long i = numb10; i > 0;)
+	{
+
+		n = i / j3;
+		i -= j3 * n;
+		j3 /= 3;
+		cout << n;
+	}*/
+	
+
+
+	//для вывода: сдвигать на один символ
+	return 0;
+}
+
+int numb(char symb)//замена символа на число
+{
+	if (symb >= 48 && symb <= 57) return symb - 48;
+	if (symb >= 65 && symb <= 67) return symb - 55;
+}
+
+
+//нормальная реализация кода
+	/*long  answer = 0;
+		int n = 0;
+		for (long long i = numb10; i > 0;)
+		{
+
+			int temp = i % 3;
+			i /= 3;
+			answer += temp * pow(10, n);
+			n++;
+		}
+		cout << "numb3 = " << answer;*/
---
+++ lab7/zad2/zad2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numb;
+	cin >> numb;
+	int n;
+	if (numb < 0)
+	{
+		cout << "1";
+		numb *= 1;
+	}
+	for (n = 0; pow(2, n) < numb; n++);
+	int n2 = pow(2, n);
+	bool B = 0;
+	if (n2 == numb)
+	{
+		B = 1;
+	}
+	int j = 0;
+	for (int i = numb; i > 0;)
+	{
+		j = i / n2;
+		i -= j * n2;
+		cout << j;
+		n2 /= 2;
+	}
+	if (B)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << "0";
+		}
+	}
+
+	return 0;
+}
+
